573931b6c9edcac3dc510931212869a4271f8b93
kamaldevkumar230-commits <kamaldevkumar230@gmail.com>
2026-03-09 23:25:38 +0530

    first commit

---
+++ src/main/java/com/portfolio/KamalPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamalPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamalPortfolioApplication.class, args);
+	}
+
+}
